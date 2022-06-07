a95012f949e5f5d9a38e9f66d5baa6addddac459
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-07 18:43:02 +0200

    Add 'description' field to the ProductListPage

--- react_webshop_fileupload/src/ProductListPage.js
+++ react_webshop_fileupload/src/ProductListPage.js
@@ -7,30 +7,41 @@ function ProductListPage() {
     // const global = localStorage.getItem("global");
 
     useEffect(() => {
-            fetch(`https://localhost:44397/GetProducts`)
-                .then(res => res.json())
-                .then(products => { setProducts(products); console.log(products); })
-                .catch(console.log);
+        fetch(`https://localhost:44397/GetProducts`)
+            .then(res => res.json())
+            .then(products => { setProducts(products); console.log(products); })
+            .catch(console.log);
     }, []);
 
     return (
         <div className='text-center mt-2'>
             <h2>Available products </h2>
             {products.map(product => (
-                <div className='row border-bottom' key={product.id}>
-                    <div className='col-sm-4'>
+                <div className='row border-bottom p-1' key={product.id}>
+                    <div className='col-sm-4 align-self-center'>
                         <NavLink to={"/product/" + product.id} >
                             <img className='img-fluid p-2' src={`https://localhost:44397/images/${product.imglink}` ? `https://localhost:44397/images/${product.imglink}` : "https://via.placeholder.com/400x400"} alt={product.name} />
                         </NavLink>
                     </div>
-                    <div className='col-sm-3 align-self-center'>
-                        <h5>{product.name}</h5>
+                    <div className='col-sm-2 align-self-center'>
+                        <NavLink className="no-decor" to={"/product/" + product.id} >
+                            <h5>{product.name}</h5>
+                        </NavLink>
                     </div>
-                    <div className='col-sm-2 align-self-center'>{product.price} EUR</div>
-                </div >
+                    <div className='col-sm-3 align-self-center small'>
+                        <NavLink className="no-decor" to={"/product/" + product.id} >
+                            {product.description}
+                        </NavLink>
+                    </div>
+                    <div className='col-sm-1 align-self-center'>
+                        <NavLink className="no-decor" to={"/product/" + product.id} >
+                            {product.price} EUR
+                        </NavLink>
+                    </div>
+                </div>
             ))
             }
-        </div >
+        </div>
     )
 }
 
